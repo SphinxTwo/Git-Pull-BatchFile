809ba5605fb475493fe0d949b9a0d035a14dd789
SphinxTwo <coddude21@hotmail.com>
2017-10-26 17:03:45 -0400

    modified: Added Variables

--- git.bat
+++ git.bat
@@ -1,8 +1,9 @@
 @echo off
 Title %DATE% time/t
+set subl1 = "E:\Program Files\Sublime Text 3\subl"
 cd /d "E:\Users\User\Downloads\Red-DiscordBot"
 git pull >%USERPROFILE%\Desktop\git-pull.txt 
 echo %DATE% >>%USERPROFILE%\Desktop\git-pull.txt
 time /T >>%USERPROFILE%\Desktop\git-pull.txt
-"E:\Program Files\Sublime Text 3\subl" %USERPROFILE%\Desktop\git-pull.txt
+subl1 %USERPROFILE%\Desktop\git-pull.txt
 exit
