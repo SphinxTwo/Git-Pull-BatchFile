dd0f08d5a3117d8b84b8651a0ab5a7ed8d27381b
SphinxTwo <coddude21@hotmail.com>
2017-10-26 17:18:01 -0400

    removed text

--- git.bat
+++ git.bat
@@ -3,7 +3,6 @@ Title %DATE% time/t
 set subl1 = "E:\Program Files\Sublime Text 3\subl"
 set /p RedLocation = "Enter Red Location"
 cd /d "E:\Users\User\Downloads\Red-DiscordBot"
-"mer"
 git pull >%USERPROFILE%\Desktop\git-pull.txt
 echo %DATE% >>%USERPROFILE%\Desktop\git-pull-red.txt
 time /T >>%USERPROFILE%\Desktop\git-pull-red.txt
